13bda2692ecc7c6e40ea586a2252c14a9bc9ae46
jordan 208 <jordan@hoomiz.com>
2024-11-07 18:57:02 +0100

    persistor

--- src/index.js
+++ src/index.js
@@ -5,16 +5,14 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
-import { store, persistor } from './store';
+import { store } from './store';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <Provider store={store}>
-  <PersistGate loading={null} persistor={persistor}>
   <React.StrictMode>
-    <App />
+    <App /> 
   </React.StrictMode>
-  </PersistGate>
   </Provider>
 );
 
--- src/store.js
+++ src/store.js
@@ -1,17 +1,5 @@
 import { createStore } from 'redux';
-import { persistStore, persistReducer } from 'redux-persist';
-import storage from 'redux-persist/lib/storage';
-
-// Importez vos reducers ici
 import rootReducer from './reducers';
 
-// Configuration de Redux Persist
-const persistConfig = {
-  key: 'root',
-  storage,
-};
-
-// Créer le store Redux avec le reducer racine et Redux Persist
-const persistedReducer = persistReducer(persistConfig, rootReducer);
-export const store = createStore(persistedReducer);
-export const persistor = persistStore(store);
+// Créer le store Redux avec le reducer racine
+export const store = createStore(rootReducer);
